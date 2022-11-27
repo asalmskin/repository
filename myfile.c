ae7a1a8a8098c652f1c40fb23cc780358098c4c3
asalmskin <asalmsk82@gmail.com>
2022-11-27 19:25:20 +0330

    myfile.c has a loop

--- myfile.c
+++ myfile.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
 int main () {
-	int num, num2;
+	int num, num2, n;
 	printf("hi this is a simple code!\n");
-	printf("please enter your numbers:\n");
-	scanf("%d%d", &num, &num2);
-	printf("the answer is %d\n", num + num2);
+	printf("how many times?\n");
+	scanf("%d", &n);
+	for(int i = 0, i < n; i++) {
+		printf("please enter your numbers:\n");
+		scanf("%d%d", &num, &num2);
+		printf("the answer is %d\n", num + num2);
+	}
 	return 0;
 }
