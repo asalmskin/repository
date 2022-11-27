922c64595e85b3474cec4accbee5abe64f7efe9e
asalmskin <asalmsk82@gmail.com>
2022-11-27 19:21:27 +0330

    myfile.c can add two numbers.

--- myfile.c
+++ myfile.c
@@ -3,7 +3,8 @@
 int main () {
 	int num, num2;
 	printf("hi this is a simple code!\n");
-	printf("please enter your number:\n");
-	scanf("%d", &num);
+	printf("please enter your numbers:\n");
+	scanf("%d%d", &num, &num2);
+	printf("the answer is %d\n", num + num2);
 	return 0;
 }
